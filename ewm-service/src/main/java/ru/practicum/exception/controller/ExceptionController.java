0e3bdae84453a724fd73b4d7b7ead00d746f63e3
Evdokimov A <alexandrevd2107@gmail.com>
2025-07-31 01:40:58 +0300

    implemented the reviewer's comments fix 500

--- ewm-service/src/main/java/ru/practicum/exception/controller/ExceptionController.java
+++ ewm-service/src/main/java/ru/practicum/exception/controller/ExceptionController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,6 +18,30 @@ import java.util.NoSuchElementException;
 @RestControllerAdvice
 @Slf4j
 public class ExceptionController {
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handle(MissingServletRequestParameterException exc) {
+        return new ApiError(
+                null,
+                "Required parameter '" + exc.getParameterName() + "' is not present",
+                "Validation failed",
+                "400",
+                LocalDateTime.now()
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handle(MethodArgumentNotValidException exc) {
+        return new ApiError(
+                null,
+                "Invalid request data",
+                "Validation failed",
+                "400",
+                LocalDateTime.now()
+        );
+    }
 
     @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -112,18 +137,6 @@ public class ExceptionController {
                 exc.getMessage(),
                 "Internal server error.",
                 "500",
-                LocalDateTime.now()
-        );
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handle(MethodArgumentNotValidException exc) {
-        return new ApiError(
-                null,
-                "Invalid request data",
-                "Validation failed",
-                "400",
                 LocalDateTime.now()
         );
     }
